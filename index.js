a90894e75bfa4249a748fd9c26e418500e778402
zhouyang20 <zhouyang20@58ganji.com>
2019-05-03 14:56:40 +0800

    fix:error

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-/**
- * 脚手架入口文件
- * zhouyang20
- * 2019.04.29
- */
-
-//可以自动的解析命令和参数，用于处理用户输入的命令。
 const program = require('commander');
-//下载并提取 git 仓库，用于下载项目模板
 const download = require('download-git-repo');
-//交互式命令，向用户提问题
 const inquirer = require('inquirer');
-//将 handlebars 渲染完后的模板重新写入到文件中
 const fs = require('fs');
-//使用 ora 来提示用户正在下载中
 const ora = require('ora');
-//通过 chalk 来为打印信息加上样式，比如成功信息为绿色，失败信息为红色，这样子会让用户更加容易分辨，同时也让终端的显示更加的好看
 const chalk = require('chalk');
-//使用 log-symbols 在信息前面加上 √ 或 × 等的图标
 const symbols = require('log-symbols');
-//模板引擎，将用户提交的信息动态填充到文件中。
 const handlebars = require('handlebars');
 
 program.version('1.0.0', '-v, --version')
